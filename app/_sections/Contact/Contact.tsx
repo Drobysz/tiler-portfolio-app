ac2ec00e1e1d39b40345d92acda80d0bd1a626f2
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 18:25:23 +0200

    feat(section): add contact call-to-action

---
+++ app/_sections/Contact/Contact.tsx
@@ -0,0 +1,48 @@
+import { GlidingImageBackground, MainBtn } from "@/components"
+import s from "./style.module.scss";
+import { SectContainer } from "@/components/containers/SectContainer/SectContainer";
+import { SplitText } from "@/components/animations/Texts/SplitText/SplitText";
+import { SplitByRowsText } from "@/components/animations/Texts/SplitByRowsText/SplitByRowsText";
+import { ViewReveal } from "@/components/animations/ViewReveal/ViewReveal";
+
+export const Contact = ()=> {
+    return (
+        <GlidingImageBackground
+            img_url="/poster.jpg"
+            withShadow
+            className="w-full h-[130vh] rounded-t-2xl translate-y-1"
+        >
+            <SectContainer
+                className="flex flex-col gap-50 z-10 py-20"
+            >
+                <div className="flex flex-col items-center">
+                    <SplitByRowsText 
+                        className={s.subtitle}
+                        tag="p"
+                    >
+                        Création de votre confort avec
+                    </SplitByRowsText> 
+                    <SplitText 
+                        className={s.title}
+                        tag="h2"
+                    >
+                        D.P Carrelage
+                    </SplitText>
+                </div>
+
+                <ViewReveal className="flex flex-col items-center gap-14">
+                    <p className={s.desc}>
+                        Si mes services vous intéressent en Franche-Comté, je suis toujours à votre disposition.
+                    </p>
+                    <MainBtn
+                        withArrow
+                        size="lg"
+                        color="white"
+                    >
+                        Contact
+                    </MainBtn>
+                </ViewReveal>
+            </SectContainer>
+        </GlidingImageBackground>
+    )
+}
