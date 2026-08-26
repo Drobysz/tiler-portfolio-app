d4afadc04101e9dc4c2cae4d778e1ca210793df0
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:31:23 +0200

    chore: configure portfolio dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    globalNotFound: true,
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
