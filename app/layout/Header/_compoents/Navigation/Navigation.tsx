df8716d3c53586e7af969e562f194723bf08afac
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:32:43 +0200

    feat(header): add primary navigation

---
+++ app/layout/Header/_compoents/Navigation/Navigation.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import s from "./style.module.scss";
+import { ReactNode, useState } from "react";
+import Link from "next/link";
+
+const NavElement = ({
+    isNarrowed,
+    isMainPage,
+    href,
+    className,
+    children, 
+}: {
+    isNarrowed: boolean;
+    isMainPage: boolean;
+    href: string;
+    className?: string;
+    children: ReactNode;
+})=> {
+    const [hover, setHover] = useState(false);
+
+    return (
+        <li 
+            className={cn(
+                className,
+                s.nav_element, {
+                    ["before:bg-white text-white"]: isNarrowed,
+                    ["before:bg-black text-black"]: !isNarrowed,
+                }
+            )}
+            onMouseEnter={()=> setHover(true)}
+            onMouseLeave={()=> setHover(false)}
+        >
+            <Link
+                href={href}
+            >
+                <span className="whitespace-nowrap">
+                    {children}
+                </span>
+                <span
+                    className={cn(
+                        "duration-600 ease-out transition-transform font-medium",
+                        "absolute left-1/2 whitespace-nowrap -translate-x-1/2", {
+                            ["text-black"]: isNarrowed,
+                            ["text-white"]: !isNarrowed,
+                            ["translate-y-0"]: hover,
+                            ["translate-y-10"]: !hover,
+                        }
+                    )}
+                >
+                    {children}
+                </span>
+            </Link>
+        </li>
+    )
+}
+
+
+export const Navigation = ({
+    isNarrowed,
+    isMainPage,
+}: {
+    isNarrowed: boolean;
+    isMainPage: boolean;
+})=> {
+    const pages = [
+        { label: "Main Page", href: "/" },
+        { label: "Projets", href: "/projects" },
+        { label: "Contacts", href: "/contacts" },
+    ];
+
+    return (
+        <nav>
+            <ul className="inline-flex gap-3">
+                {pages.map((p, i)=>
+                    <NavElement
+                        key={`nav-item-${i}`}
+                        isNarrowed={isNarrowed}
+                        isMainPage={isMainPage}
+                        href={p.href}
+                    >
+                        {p.label}
+                    </NavElement>
+                )}
+            </ul>
+        </nav>
+    )
+}
