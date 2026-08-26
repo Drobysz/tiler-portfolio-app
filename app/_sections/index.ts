21714780670f911ef3d3bdc8de324b0577f85fab
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:32:22 +0200

    chore(section): export homepage sections

---
+++ app/_sections/index.ts
@@ -0,0 +1,5 @@
+export * from "./Hero/Hero";
+export * from "./Quote/Quote";
+export * from "./Values/Values";
+export * from "./ServiceAnimationFeed/ServiceAnimationFeed";
+export * from "./WorkerAbout/WorkerAbout";
