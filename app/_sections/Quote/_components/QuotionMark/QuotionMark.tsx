7b41d61f00f4ffa87b0c0ef5b7263c5d03f8d044
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:32:21 +0200

    feat(quote): add quotation mark

---
+++ app/_sections/Quote/_components/QuotionMark/QuotionMark.tsx
@@ -0,0 +1,13 @@
+import { QuoteIcon } from "lucide-react"
+
+export const QuotionMark = ()=> {
+    return (
+        <span
+            className="inline-block"
+        >
+            <QuoteIcon
+                className="w-5 h-5"
+            />
+        </span >
+    )
+}
