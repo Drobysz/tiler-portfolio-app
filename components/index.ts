0930a391fb1f593d8dcb4836947ff73b18d349ce
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:31:46 +0200

    chore(component): export shared components

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export * from "./ImageSwitcher/ImageSwitcher";
+export * from "./Line/Line";
+export * from "./Buttons/MainBtn/MainBtn";
+export * from "./SMbar/SMbar";
+export * from "./ImageSwitcher/ImageSwitcher";
+export * from "./UnderlinedLink/UnderlinedInnerLink";
+export * from "./UnderlinedLink/UnderlinedLink";
