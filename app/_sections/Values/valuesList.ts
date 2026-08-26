ab9e2398b51bc27f53590ad9893c220cad838cfd
Drobysz <alexanderdrobyschevski@gmail.com>
2026-08-26 08:32:22 +0200

    feat(section): add values section

---
+++ app/_sections/Values/valuesList.ts
@@ -0,0 +1,19 @@
+const values = [
+    { 
+        label: "Be Great.",
+        desc: "I believe in doing great work, even when it’s hard. Show up with passion and a make-it-happen attitude every day. Greatness is a choice. Choose to be great, every day.",
+        img: "/value_tiles/tile1.jpg",
+    },
+    { 
+        label: "Make Amazing.",
+        desc: "I believe in striving for thoughtful, relevant, trend-right design in all I do. That’s why I sweat the details wherever and whenever I show up–whether in our products, our stories, or our environments. I push ourselves and our teams to make amazing.",
+        img: "/value_tiles/tile2.jpg",
+    },
+    { 
+        label: "Act Now.",
+        desc: "I believe speed, agility, and relentless problem-solving are crucial to staying ahead. Our industry and consumer move fast, and I have to move faster. Don’t wait. Let’s go!",
+        img: "/value_tiles/tile3.jpg",
+    },
+];
+
+export default values;
